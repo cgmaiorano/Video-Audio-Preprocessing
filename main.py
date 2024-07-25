baf1799359867c66cbe8059734b947f9740df34b
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2024-07-25 11:28:14 -0400

    Update main.py
    removed inline comments

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def __main__():
     if not os.path.exists(audio_output_folder):
         os.makedirs(audio_output_folder)
 
-    file_list = list_files(video_input_folder, video_format) # list_file innately has nat sort, don't think we need another sort in ln 25, check if both outputs are the same.
+    file_list = list_files(video_input_folder, video_format) 
     grouped_files = {}
 
     for file in file_list:
-        participant_id = os.path.basename(file)[:7] # Using first 7 as "Participant ID"
+        participant_id = os.path.basename(file)[:7] 
         if participant_id not in grouped_files:
             grouped_files[participant_id] = []
         grouped_files[participant_id].append(file)
 
     for participant_id, files in tqdm(grouped_files.items(), desc="Processing Participant Files"):
-        files.sort()  # Ensure the files are in the correct order if not already
-        output_filename = f'{participant_id}_concatenated.{audio_format}' # Switch to _speech_language if needed
+        files.sort() 
+        output_filename = f'{participant_id}_concatenated.{audio_format}' 
         output_path = os.path.join(audio_output_folder, output_filename)
         process_and_concatenate_videos(files, output_path, target_sample_rate)
 
